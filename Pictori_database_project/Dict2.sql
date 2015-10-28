6e14d964ff6a3c846a9a3bafee40f55e910e7af2
pictori <1pictorio@gmail.com>
2015-10-28 19:40:58 +0100

    Adatbázis tervek 1.0 Van benne példa program (nem én írtam), pdf formátumú modell, akkor egy eredeti workbench projekt és egy SQLiteman programocskában elkészített adatbázis, ami siralmasan néz ki :D, elméletileg a view-ben egy általam készített nyelv szűrő is található, ilyeneket kellene gyártani majd még. Azt hiszem ennyi, ha nem leszek ilyen fáradt holnap akkor javítok rajta, meg átvizsgálom, hogy a szerkezetet.

---
+++ Pictori_database_project/Dict2.sql
@@ -0,0 +1,15 @@
+insert into nyelvek VALUES(1, 'Magyar');
+select * from nyelvek;
+insert into szavak values(1,1,'király');
+select * from szavak;
+insert into szótárak values(1, 1, 2);
+select * from szótárak;
+insert into nyelvek values(2, 'Angol');
+insert into szavak values (2,2, 'king');
+insert into szópárok values (1,1,1,2);
+select * from szópárok;
+select * from szavak;
+select * from szavak inner join szópárok on szavak.ID_Szavak = szópárok.Szó_1_ID and ... where "király" =  szavak...;
+
+
+select Szó_neve from szavak where Nyelv_ID = (select ID_Nyelvek from  Nyelvek where Nyelv_Neve = 'Angol');
