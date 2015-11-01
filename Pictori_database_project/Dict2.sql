aa9d370980eaaf87e817b53adbe09abb29fdd957
pictori <1pictorio@gmail.com>
2015-11-01 23:24:24 +0100

    SQLiteman program és egy sqlite adatbázis, mely Máté terve alapján készült

--- Pictori_database_project/Dict2.sql
+++ Pictori_database_project/Dict2.sql
@@ -1,15 +1,40 @@
-insert into nyelvek VALUES(1, 'Magyar');
-select * from nyelvek;
-insert into szavak values(1,1,'király');
-select * from szavak;
-insert into szótárak values(1, 1, 2);
-select * from szótárak;
-insert into nyelvek values(2, 'Angol');
-insert into szavak values (2,2, 'king');
-insert into szópárok values (1,1,1,2);
-select * from szópárok;
-select * from szavak;
-select * from szavak inner join szópárok on szavak.ID_Szavak = szópárok.Szó_1_ID and ... where "király" =  szavak...;
+select word from words where  (select meaning from words where word="busz") =  (select ID from words where nyelv = "2");
+select * from words;
+insert into words values (18,"bus",   19,  2);
+insert into words values (23,"busz",   20,  1);
+
+select * from languages;
+insert into languages values(1, "Magyar");
+insert into languages values(2, "Angol");
+insert into languages values(3, "Német");
+select word from words where (nyelv = "Angol") and   ID = (select meaning from words where word="busz");
+
+select * from words where  nyelv = (select ID from  languages where Name = 'Német')  and   ID = 
+ (select meaning from words where  (word="busz" and  nyelv= (select ID from  languages where Name = 'Magyar')));
+ 
+ 
+select * from words where  nyelv = (select ID from  languages where Name = 'Német')and word like  '%us%';
+select ID from  languages where Name = 'Angol'; 
+
+select * from words where ( nyelv = (select ID from  languages where Name = 'Német')) ;
+
+select meaning from words where word="busz" and  nyelv = (select ID from  languages where Name = 'Magyar');
+
+select * from words where  nyelv = (select ID from  languages where Name = 'Német' ) and ( ID = "20");
+
+SELECT * FROM `words` WHERE Meaning = 1;
+
+SELECT * FROM `words` WHERE Meaning = (select ID from words where word="bus" and 
+nyelv = (select ID from  languages where Name = 'Német')) and
+nyelv = (select ID from  languages where Name = 'Magyar');
 
 
-select Szó_neve from szavak where Nyelv_ID = (select ID_Nyelvek from  Nyelvek where Nyelv_Neve = 'Angol');
+select nyelv from words where word="bus";
+select ID from languages where Name="Angol";
+
+select * from words;
+
+select word from words where ( nyelv = (select ID from  languages where Name = 'Német')) and  
+ ID = (select meaning from words where word="busz");
+ 
+ insert into words values (25,"autóbusz", 18, 1);
