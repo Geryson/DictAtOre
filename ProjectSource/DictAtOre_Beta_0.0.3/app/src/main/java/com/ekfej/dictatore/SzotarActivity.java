fdf7343c7377b2d585551dff3441818ff80aece7
Geryson <gcsavas@freemail.hu>
2015-11-18 22:05:17 +0100

    A Tudásteszt és a Szótár activityjének első változata, és módosítás az ütemtervben

---
+++ ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/SzotarActivity.java
@@ -0,0 +1,13 @@
+package com.ekfej.dictatore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SzotarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_szotar);
+    }
+}
