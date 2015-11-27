60fd276edbc7c4af7840bfca14cf1bfe051d941b
pictori <1pictorio@gmail.com>
2015-11-27 19:39:56 +0100

    Szótár elkezdése

--- ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/DictActivity.java
+++ ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/DictActivity.java
@@ -2,12 +2,41 @@ package com.ekfej.dictatore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class DictActivity extends AppCompatActivity {
+import com.ekfej.dictatore.Database.DatabaseAccess;
+
+import java.util.List;
+
+public class DictActivity extends AppCompatActivity implements View.OnClickListener {
+    private ListView FirstWords, SecondWords;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dict);
+
+        FirstWords = (ListView) findViewById(R.id.FirstWords);
+        SecondWords = (ListView) findViewById(R.id.SecondWords);
+
+        Bundle LangaugeBundle = getIntent().getExtras();
+        String FirstLanguageBundle = LangaugeBundle.getString("FirstLanguage");
+        String SecondLanguageBundle = LangaugeBundle.getString("SecondLanguage");
+
+        DatabaseAccess db = DatabaseAccess.getInstance(this);
+        List<String> FirstLanguageList = db.WordsSelect(FirstLanguageBundle);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, FirstLanguageList);
+        FirstWords.setAdapter(adapter);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
