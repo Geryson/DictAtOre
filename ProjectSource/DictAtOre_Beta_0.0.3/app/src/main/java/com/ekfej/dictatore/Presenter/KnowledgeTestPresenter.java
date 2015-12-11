b34b4e8a2f19a5bf4b57f2c0d6ae5a0c31a710c7
Lloydi50 <lloyd50gt@gmail.com>
2015-12-11 18:39:45 +0100

    MainPresenter.java: + StringMeaningList2WordMeaningList fgv + StringWordArray2WordWordArray fgv
    KnowledgeTestPresenter.java:
    + konstruktor
    + GetRandomWords elj
    + GetNextWord fgv

--- ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/Presenter/KnowledgeTestPresenter.java
+++ ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/Presenter/KnowledgeTestPresenter.java
@@ -2,6 +2,7 @@ package com.ekfej.dictatore.Presenter;
 
 import android.content.Context;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 /**
@@ -9,13 +10,35 @@ import java.util.List;
  */
 public class KnowledgeTestPresenter extends MainPresenter {
 
-    private byte WordsCount = 15;
-    public byte GetWordsCount() { return WordsCount; }
+    private Language language1;
+
+    private Language language2;
+
+    private Word[] words;
+    private int next = 0;
+    public Word GetNextWord() {
+        if (next < wordsCount) return words[next++];
+        else return null;
+    }
+
+    private int wordsCount = words.length;
+    public int GetWordsCount() {
+        return wordsCount;
+    }
 
 
-
-    public KnowledgeTestPresenter(Context context, String language1, String language2) {
+    public KnowledgeTestPresenter(Context context, String language1, String language2, int numberOfWords) {
         super(context);
+        this.language1 = new Language(language1);
+        this.language2 = new Language(language2);
+        GetRandomWords(numberOfWords, this.language1, this.language2);
+    }
+
+
+    private void GetRandomWords(int number, Language language1, Language language2) {
+        String[] wordsNames = db.Expression(language1.getName(), number);
+
+        words = StringWordArray2WordWordArray(number, wordsNames, language1, language2);
     }
 
 
--- ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/Presenter/MainPresenter.java
+++ ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/Presenter/MainPresenter.java
@@ -4,6 +4,9 @@ import android.content.Context;
 
 import com.ekfej.dictatore.Database.DatabaseAccess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Lloyd on 2015. 12. 11..
  */
@@ -11,8 +14,46 @@ public class MainPresenter {
 
     protected DatabaseAccess db;
 
+
+
     protected MainPresenter(Context context) {
         db = DatabaseAccess.getInstance(context);
+    }
+
+
+
+    protected List<Word> StringMeaningList2WordMeaningList(List<String> wordsNames, Language language){
+        List<Word> words = new ArrayList<Word>();
+        int wordsNamesSize = wordsNames.size();
+
+        for (int i = 0; i < wordsNamesSize; i++) {
+            words.add(new Word(wordsNames.get(i), language));
+        }
+
+        return words;
+    }
+
+    protected Word[] StringWordArray2WordWordArray(int number, String[] wordsNames, Language language1, Language language2) {
+        Word[] words = new Word[number];
+        String language1Name = language1.getName();
+        String language2Name = language2.getName();
+
+        for (int i = 0; i < number; i++) {
+            words[i] = new Word(
+                    wordsNames[i],
+                    StringMeaningList2WordMeaningList(
+                            db.Decipherment(
+                                    language2Name,
+                                    language1Name,
+                                    wordsNames[i]
+                            ),
+                            language2
+                    ),
+                    language1
+            );
+        }
+
+        return words;
     }
 
 }
