335a721e685e106f884fe37e666ce958cf9ddd55
Lloydi50 <lloyd50gt@gmail.com>
2015-12-31 16:19:35 +0100

    DictPresenter.java: + konstruktor bővítése + languageChanged fgv + insertLanguage2Adapter elj + swapLanguages elj - language1Changed fgv - language2Changed fgv - insertLanguage2SelectableLanguages elj

--- ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/Presenter/DictPresenter.java
+++ ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/Presenter/DictPresenter.java
@@ -1,6 +1,7 @@
 package com.ekfej.dictatore.Presenter;
 
 import android.content.Context;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,61 +15,91 @@ public class DictPresenter extends MainPresenter {
 
     private Language language2;
 
-    private List<Language> allLanguages;
+    private List<Language> adapter;
 
-    private List<Language> selectableLanguages;
+    private Spinner spinner1;
+
+    private Spinner spinner2;
 
 
 
-    public DictPresenter(Context context, int language1Index, int language2Index) {
+    public DictPresenter(Context context, int language1Index, int language2Index, List<Language> adapter, Spinner spinner1, Spinner spinner2) {
         super(context);
-        allLanguages = stringLanguageList2LanguageLanguageList(db.lister.LanguageSelect());
-        language1 = allLanguages.get(language1Index);
-        language2 = allLanguages.get(language2Index);
+        this.adapter = adapter;
+        this.spinner1 = spinner1;
+        this.spinner2 = spinner2;
+
+        adapter = stringLanguageList2LanguageLanguageList(db.lister.LanguageSelect());
+
+        language1 = adapter.get(language1Index);
+        language2 = adapter.get(language2Index);
+
+        spinner1.setSelection(language1Index);
+        spinner2.setSelection(language2Index);
+
+        adapter.remove(language1Index);
+        adapter.remove(language2Index);
     }
+
 
 
     /**
      * Visszaadja a kiválasztott első nyelv szavait és második nyelvű jelentéseit.
+     * @param spinnerId A spinner id-je ahol a változás történt.
      * @param selectedLanguageIndex A kiválasztott elem sor indexe.
-     * @param adapter A választható nyelveket tartalmazó adapter.
      * @return Egy Word típusú tömböt ad vissza.
      */
-    public Word[] language1Changed(int selectedLanguageIndex, List<Language> adapter){
-        selectableLanguages.remove(selectedLanguageIndex);
-        insertLanguage2SelectableLanguages(language1);
-        language1 = adapter.get(selectedLanguageIndex);
-        adapter = selectableLanguages;
+    public Word[] languageChanged(int spinnerId, int selectedLanguageIndex) {
+        if (spinnerId == 0) {
+            Language old = language1;
+            language1 = adapter.get(selectedLanguageIndex);
+
+            adapter.remove(selectedLanguageIndex);
+            insertLanguage2Adapter(old);
+        }
+        else {
+            Language old = language2;
+            language2 = adapter.get(selectedLanguageIndex);
+
+            adapter.remove(selectedLanguageIndex);
+            insertLanguage2Adapter(old);
+        }
 
         return words2Display(language1, language2);
     }
 
-    public Word[] language2Changed(int selectedLanguageIndex, List<Language> adapter){
-        selectableLanguages.remove(selectedLanguageIndex);
-        insertLanguage2SelectableLanguages(language2);
-        language2 = adapter.get(selectedLanguageIndex);
-        adapter = selectableLanguages;
-
-        return words2Display(language1, language2);
-    }
-
-    private void insertLanguage2SelectableLanguages(Language lang) {
+    private void insertLanguage2Adapter(Language lang) {
         int langId = lang.getId();
 
         int i = 0;
-        while (selectableLanguages.get(i).getId() < langId) {
+        while (adapter.get(i).getId() < langId) {
             i++;
         }
-        selectableLanguages.add(i, lang);
+        adapter.add(i, lang);
     }
 
-    public Word[] words2Display(Language language1, Language language2) {
+
+    public void swapLanguages() {
+        Spinner temp;
+        temp = spinner1;
+        spinner1 = spinner2;
+        spinner2 = temp;
+
+        int tempId;
+        tempId = spinner1.getId();
+        spinner1.setId(spinner2.getId());
+        spinner2.setId(tempId);
+    }
+
+    private Word[] words2Display(Language language1, Language language2) {
         return (Word[])db.wordMethod.DictionarySelect(language1.getName(), language2.getName()).toArray();
     }
+
 
     public void addWord(Word word) {
         db.wordMethod.WordInsert(word);
     }
+
 
     public void deleteWord(Word word) {
         db.wordMethod.WordDelete(ideiglenesAtalakitoDeleteWordhoz(word));
