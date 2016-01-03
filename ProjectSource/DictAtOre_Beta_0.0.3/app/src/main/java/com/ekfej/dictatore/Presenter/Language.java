e7160bd1af3b286b2b1e8cacb85de081a4c84ce9
Lloydi50 <lloyd50gt@gmail.com>
2016-01-03 19:28:27 +0100

    Language.java: + Parcelable implementálása

--- ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/Presenter/Language.java
+++ ProjectSource/DictAtOre_Beta_0.0.3/app/src/main/java/com/ekfej/dictatore/Presenter/Language.java
@@ -1,9 +1,12 @@
 package com.ekfej.dictatore.Presenter;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Lloyd on 2015. 11. 27..
  */
-public class Language {
+public class Language implements Parcelable {
 
     private int id;
     public int getId() {
@@ -37,4 +40,34 @@ public class Language {
     public String toString() {
         return name;
     }
+
+
+    protected Language(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Language> CREATOR = new Parcelable.Creator<Language>() {
+        @Override
+        public Language createFromParcel(Parcel in) {
+            return new Language(in);
+        }
+
+        @Override
+        public Language[] newArray(int size) {
+            return new Language[size];
+        }
+    };
 }
